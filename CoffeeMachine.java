1cd5376b859da8d9acb90d2cf06f3dfd4b0a001b
Patricia Rodríguez <prlongoria@gmail.com>
2022-08-21 08:43:39 +0200

    init project

---
+++ CoffeeMachine.java
@@ -0,0 +1,26 @@
+class CoffeeMachine {
+    public static void main(String[] args) {
+    
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+    }
+} 
+// Print in terminal:
+// Starting to make a coffee
+
+// Grinding coffee beans
+
+// Boiling water
+
+// Mixing boiled water with crushed coffee beans
+
+// Pouring coffee into the cup
+
+// Pouring some milk into the cup 
+
+// Coffee is ready!
